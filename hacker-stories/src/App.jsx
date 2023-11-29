9a2af3cf51b978501c906e5b462aa43bf2932a3e
jose-foronda <jose09621@hotmail.com>
2023-11-29 00:16:45 -0500

    react fragments

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -82,14 +82,14 @@ const Item = ({ item }) => {
 }
 
 const Search = ({ search, onSearch }) => {
-  return (<div>
+  return (<>
     <label htmlFor="search">Search: </label>
     <input
       id="search"
       type="text"
       value={search}
       onChange={onSearch} />
-  </div>
+  </>
   )
 };
 
